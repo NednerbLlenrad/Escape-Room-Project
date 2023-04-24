2a68ae385ee39aeefa8c294d8557416cb665d2a4
NednerbLlenrad <brotherbrenny@gmail.com>
2023-04-24 15:27:30 -0700

    Added countdown

--- Main.cpp
+++ Main.cpp
@@ -13,6 +13,17 @@ int main()
         return EXIT_FAILURE;
     }
 
+    //Countdown
+    sf::Font font;
+    font.loadFromFile("Font/joystix monospace.ttf");
+    sf::Text countdownTxt("", font, 50);
+    countdownTxt.setPosition(0, 0);
+    //Create clock for countdown
+    sf::Clock clock;
+    //Countdown Value
+    float countdownTime = 61;
+
+
     // create a sprite and set its texture
     sf::Sprite player(texture);
     player.setTextureRect(sf::IntRect(0, 0, 128, 128)); //set visible texture to only first sprite on sheet
@@ -26,14 +37,6 @@ int main()
     backgroundTexture.loadFromFile("images/background.png");
     background.setTexture(&backgroundTexture);
 
-    //draw background
-    window.draw(background);
-
-    // draw the sprite in the window
-    window.draw(player);
-
-    // display the window
-    window.display();
 
     // wait for the window to be closed
     while (window.isOpen()) {
@@ -42,6 +45,31 @@ int main()
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+        }
+
+        // Update the Countdown
+        sf::Time elapsed = clock.restart();
+        countdownTime -= elapsed.asSeconds();
+        int countdownInt = static_cast<int>(countdownTime); // Convert the float to an integer
+        countdownTxt.setString(std::to_string(countdownInt)); // Set the text to the integer string
+
+        //draw background
+        window.draw(background);
+
+        // draw the sprite in the window
+        window.draw(player);
+
+        //draws the countdown
+        window.draw(countdownTxt);
+
+        // display the window
+        window.display();
+
+        //Countdown reached result presumably due to a failed attempt
+        if (countdownTime <= 0)
+        {
+            //Close the game? Return to menu? Fail screen?
+            window.close();
         }
     }
 
